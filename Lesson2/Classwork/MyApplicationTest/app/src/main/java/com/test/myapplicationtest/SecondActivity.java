f615c7bf9630a80dc6cee959ba2268fd339a3553
Victor <victorchuholskiy@hotmail.com>
2015-11-11 21:23:14 +0300

    lesson2
    work

---
+++ Lesson2/Classwork/MyApplicationTest/app/src/main/java/com/test/myapplicationtest/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.test.myapplicationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by USER on 11.11.2015.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
